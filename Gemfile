bcb0cf195d26f462f584c8e437132e18271484b3
EduLongen <e.lcorrea256@gmail.com>
2022-08-15 15:43:58 -0300

    seo friendly url

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ group :test do
 end
 
 gem "ransack", "~> 3.2"
+
+gem "friendly_id", "~> 5.4"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
+  extend FriendlyId
   validates :title, presence: true, length: { minimun: 5, maximum: 255 }
   validates :content, presence: true
+
   belongs_to :user
+
   has_rich_text :content
   has_many :comments, dependent: :destroy
+
+  friendly_id :title, use: %i[slugged history finders]
+
+  def should_generate_new_friendly_id?
+    title_changed? || slug.blank?
+  end
+
 end
