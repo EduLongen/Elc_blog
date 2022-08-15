7705f77e4fb9275fea7f007d84ebc454d65d9107
EduLongen <e.lcorrea256@gmail.com>
2022-08-15 14:31:33 -0300

    add search functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: "Eduardo", email: "du@test.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Eduardo", email: "eduardo@test.com", password: "123456", password_confirmation: "123456")
 
 User.create(name: "john", email: "john@test.com", password: "123456", password_confirmation: "123456")
 
 1.times do |i|
-  Post.create(title: "Post #{i}", content: "Content #{i} of post #{i}", user_id: User.first.id)
+  Post.create(title: "My first post", content: "Content of my post #{i}", user_id: User.first.id)
 end
 
 1.times do |i|
-  Post.create(title: "Post #{i}", content: "Content #{i} of post #{i}", user_id: User.second.id)
+  Post.create(title: "Post from John", content: "Content #{i} of post #{i}", user_id: User.second.id)
 end
